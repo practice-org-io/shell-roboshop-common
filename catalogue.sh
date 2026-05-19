a890ca1f333cd203e01c86fef6eafe237b93d1dc
lokesh <lokesh.theofficial@gmail.com>
2026-05-19 10:40:29 +0530

    some change

--- catalogue.sh
+++ catalogue.sh
@@ -9,11 +9,11 @@ nodejs_setup
 
 #loading data into mongodb
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
 
 INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $INDEX -lt 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
     VALIDATE $? "Loading Products"
 else
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Products are already loaded ... $Y SKIPPING $N"
